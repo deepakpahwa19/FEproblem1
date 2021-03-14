ab0b79ec3b272ffaa1517a45f1ebb3f85d1abb88
dpahawa <deepak_pahawa@intuit.com>
2021-03-14 16:56:11 +0530

    Header h1 - h6 css changes

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -56,6 +56,11 @@ const GlobalStyles = createGlobalStyle`
     font-size: 1.2rem
   }
 
+  h1, h2, h3, h4, h5, h6 {
+    text-align: center;
+    margin: 10px auto;
+  }
+
 
 `;
 
--- src/views/CommonUI/HeaderGroup.jsx
+++ src/views/CommonUI/HeaderGroup.jsx
@@ -1,33 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export const H4 = React.memo(({ children, align }) => {
-    return <H4UI align={align}>{children}</H4UI>;
+export const H4 = React.memo(({ children }) => {
+    return <H4UI>{children}</H4UI>;
 });
 
-export const H1 = React.memo(({ children, align }) => {
-    return <H1UI align={align}>{children}</H1UI>;
+export const H1 = React.memo(({ children }) => {
+    return <H1UI>{children}</H1UI>;
 });
 
-export const H3 = React.memo(({ children, align }) => {
-    return <H3UI align={align}>{children}</H3UI>;
+export const H3 = React.memo(({ children }) => {
+    return <H3UI>{children}</H3UI>;
 });
 
-const HGroup = styled.h4`
-    text-align: ${({ align }) => align || 'center'};
-    margin: ${({ margin }) => margin || '10px auto'};
-    font-weight: 700;
-`;
+const H1UI = styled.h1``;
 
-const H1UI = styled(HGroup)`
-    font-size: 3.2rem;
-`;
+const H3UI = styled.h3``;
 
-const H3UI = styled(HGroup)`
-    font-size: 1.8rem;
-    font-weight: 400;
-`;
-
-const H4UI = styled(HGroup)`
-    font-size: 1.6rem;
-`;
+const H4UI = styled.h4``;

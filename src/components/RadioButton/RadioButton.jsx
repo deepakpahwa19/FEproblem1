091ce58c59e364035abf7b6ff61a6d116385a0e4
Deepak Pahawa <deepak.pahawa@dreamorbit.com>
2022-05-22 23:27:56 +0530

    folder structure changes

---
+++ src/components/RadioButton/RadioButton.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+export const RadioButton = React.memo(({ name, value, isChecked, isDisabled, onChangeHandler, label }) => {
+    return (
+        <Label>
+            <Input
+                type='radio'
+                name={name}
+                value={value}
+                checked={isChecked}
+                onChange={onChangeHandler}
+                disabled={isDisabled}
+            />
+            {label}
+        </Label>
+    );
+});
+
+RadioButton.propTypes = {
+    name: PropTypes.string,
+    value: PropTypes.any,
+    isChecked: PropTypes.bool,
+    isDisabled: PropTypes.bool,
+    onChangeHandler: PropTypes.func,
+    label: PropTypes.string
+};
+
+const Label = styled.label`
+    padding: 2px;
+`;
+const Input = styled.input`
+    margin-right: 10px;
+`;
